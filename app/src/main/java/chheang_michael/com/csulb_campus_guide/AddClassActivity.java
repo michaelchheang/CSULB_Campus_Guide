529eadc2d4ea4583442408db92afcf3e5364f8be
Michael Chheang <michaelchheang@gmail.com>
2018-10-12 15:03:49 -0700

    Made the days of class fragment take the checkbox selections and replaces the text in the proper EditTextView

--- app/src/main/java/chheang_michael/com/csulb_campus_guide/AddClassActivity.java
+++ app/src/main/java/chheang_michael/com/csulb_campus_guide/AddClassActivity.java
@@ -1,8 +1,12 @@
 package chheang_michael.com.csulb_campus_guide;
 
+import android.app.DialogFragment;
+import android.app.Fragment;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -13,15 +17,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class AddClassActivity extends AppCompatActivity {
+public class AddClassActivity extends AppCompatActivity implements selectDaysOfClassFragment.DaysOfClassListener {
 
     AutoCompleteTextView addClassInput;
     AutoCompleteTextView addBuildingInput;
     EditText roomNumberInput;
     EditText startTimeInput;
     EditText endTimeInput;
-    CheckedTextView dateInput;
+    EditText dateInput;
     Button addToSchedule;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,7 @@ public class AddClassActivity extends AppCompatActivity {
         addBuildingInput.setAdapter(buildingsAdapter);
         addBuildingInput.setThreshold(1);
 
+        // startTimeInput will hold the user's entry for the class's start time. The user will use a TimePickerDialog to select the time.
         startTimeInput = (EditText) findViewById(R.id.startTimeEditText);
         startTimeInput.setFocusable(false);
         startTimeInput.setOnClickListener(new View.OnClickListener() {
@@ -50,12 +56,66 @@ public class AddClassActivity extends AppCompatActivity {
                 TimePickerDialog startTimePicker = new TimePickerDialog( v.getContext(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        startTimeInput.setText(hourOfDay + ":" + minute);
+                        if(hourOfDay == 0){
+                            startTimeInput.setText("12" + ":" + String.format("%1$02d", minute) + " AM");
+                        }
+                        else if(hourOfDay <= 11){
+                            startTimeInput.setText(hourOfDay + ":" + String.format("%1$02d", minute) + " AM");
+                        }
+                        else if(hourOfDay == 12){
+                            startTimeInput.setText(hourOfDay + ":" + String.format("%1$02d", minute) + " PM");
+                        }
+                        else {
+                            startTimeInput.setText(hourOfDay - 12 + ":" + String.format("%1$02d", minute) + " PM");
+                        }
                     }
                 }, 0,0,false);
                 startTimePicker.setTitle("Select Class Start Time");
                 startTimePicker.show();
             }
         });
+
+        // endTimeInput will hold the user's entry for the class's end time. The user will use a TimePickerDialog to select the time.
+        endTimeInput = (EditText) findViewById(R.id.endTimeEditText);
+        endTimeInput.setFocusable(false);
+        endTimeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog endTimePicker = new TimePickerDialog( v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if(hourOfDay == 0){
+                            endTimeInput.setText("12" + ":" + String.format("%1$02d", minute) + " AM");
+                        }
+                        else if(hourOfDay <= 11){
+                            endTimeInput.setText(hourOfDay + ":" + String.format("%1$02d", minute) + " AM");
+                        }
+                        else if(hourOfDay == 12){
+                            endTimeInput.setText(hourOfDay + ":" + String.format("%1$02d", minute) + " PM");
+                        }
+                        else {
+                            endTimeInput.setText(hourOfDay - 12 + ":" + String.format("%1$02d", minute) + " PM");
+                        }
+                    }
+                }, 0,0,false);
+                endTimePicker.setTitle("Select Class end Time");
+                endTimePicker.show();
+            }
+        });
+
+        dateInput = (EditText) findViewById(R.id.daysOfClassEditText);
+        dateInput.setFocusable(false);
+        dateInput.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new selectDaysOfClassFragment();
+                newFragment.show(getFragmentManager(), "class_days");
+            }
+        });
+    }
+
+    @Override
+    public void applyDaysString(String s) {
+        dateInput.setText(s);
     }
 }
