a187885fc5675ce44f4cab5c0fe67821665f0b12
Michael Chheang <michaelchheang@gmail.com>
2018-11-07 10:10:44 -0800

    added validation for the user input for adding a class to their schedule

--- app/src/main/java/chheang_michael/com/csulb_campus_guide/AddClassActivity.java
+++ app/src/main/java/chheang_michael/com/csulb_campus_guide/AddClassActivity.java
@@ -6,20 +6,24 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddClassActivity extends AppCompatActivity implements selectDaysOfClassFragment.DaysOfClassListener {
 
     public static final String RESULT_COURSE_INFO = "chheang_michael.com.csulb_campus_guide.AddClassActivity - Return Course Info";
     AutoCompleteTextView addClassInput;
     AutoCompleteTextView addBuildingInput;
+    EditText classNumberInput;
     EditText roomNumberInput;
     EditText startTimeInput;
     EditText endTimeInput;
@@ -38,6 +42,8 @@ public class AddClassActivity extends AppCompatActivity implements selectDaysOfC
                 (this,android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.classes));
         addClassInput.setAdapter(classesAdapter);
         addClassInput.setThreshold(1);
+
+        classNumberInput = (EditText) findViewById(R.id.classNumberEditText);
 
         // addBuildingInput will ask the user to enter the course's building (e.g. "ECS" or "Engineering and Computer Science")
         addBuildingInput = (AutoCompleteTextView) findViewById(R.id.buildingsAutoCompleteTextView);
@@ -119,22 +125,84 @@ public class AddClassActivity extends AppCompatActivity implements selectDaysOfC
         addToSchedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // Extract data from the UI:
+                // Extract data from the dialog:
                 ArrayList<String> arrayList = new ArrayList<String>();
 
-                arrayList.add("Course: " + addClassInput.getText().toString());
-                arrayList.add("Building: " + addBuildingInput.getText().toString());
-                arrayList.add("Room: " + roomNumberInput.getText().toString());
-                arrayList.add("Start Time: " + startTimeInput.getText().toString());
-                arrayList.add("End Time: " + endTimeInput.getText().toString());
-                arrayList.add("Days of Class: " + dateInput.getText().toString());
+                String course = addClassInput.getText().toString();
+                if(Arrays.binarySearch(getResources().getStringArray(R.array.classes), course) > 0){
+                    // do nothing if course matches a class from array
+                }
+                else{
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter a valid course", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
 
-                // Pass data back to schedule activity
-                Intent intent = new Intent();
-                intent.putExtra(RESULT_COURSE_INFO, arrayList);
-                setResult(Activity.RESULT_OK, intent);
-                finish();
+                String courseNumber = classNumberInput.getText().toString();
+                if(TextUtils.isEmpty(courseNumber)){
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter course number.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                String building = addBuildingInput.getText().toString();
+                if(Arrays.binarySearch(getResources().getStringArray(R.array.buildings), building) > 0) {
+                    // do nothing if input matches a building in array
+                }
+                else{
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter a valid building.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                String room = roomNumberInput.getText().toString();
+                if(TextUtils.isEmpty(room)){
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter room number.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                String startTime = startTimeInput.getText().toString();
+                if(TextUtils.isEmpty(startTime)){
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter start time.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                String endTime = endTimeInput.getText().toString();
+                if(TextUtils.isEmpty(endTime)){
+                    Toast.makeText(AddClassActivity.this,
+                            "Please enter end time.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                String daysOfWeek = dateInput.getText().toString();
+                if(TextUtils.isEmpty(daysOfWeek)){
+                    Toast.makeText(AddClassActivity.this,
+                            "Select days of class meetings.", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+                else {
+                    arrayList.add("Course: " + course + " " + courseNumber);
+                    arrayList.add("Building: " + building);
+                    arrayList.add("Room: " + room);
+                    arrayList.add("Start Time: " + startTime);
+                    arrayList.add("End Time: " + endTime);
+                    arrayList.add("Days of Class: " + daysOfWeek);
+
+                    // Pass valid data back to schedule activity
+                    Intent intent = new Intent();
+                    intent.putExtra(RESULT_COURSE_INFO, arrayList);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }
             }
         });
 
--- app/src/main/java/chheang_michael/com/csulb_campus_guide/ClassScheduleActivity.java
+++ app/src/main/java/chheang_michael/com/csulb_campus_guide/ClassScheduleActivity.java
@@ -1,20 +1,14 @@
 package chheang_michael.com.csulb_campus_guide;
 
 import android.app.Activity;
-import android.app.DialogFragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import java.util.ArrayList;
 
@@ -77,11 +71,6 @@ public class ClassScheduleActivity extends AppCompatActivity{
                 else if(resultCode == Activity.RESULT_CANCELED){
                     Toast.makeText(ClassScheduleActivity.this,
                             "Canceled!", Toast.LENGTH_LONG)
-                            .show();
-                }
-                else if(resultCode == Activity.RESULT_FIRST_USER){
-                    Toast.makeText(ClassScheduleActivity.this,
-                            "Invalid or missing information!", Toast.LENGTH_LONG)
                             .show();
                 }
                 else{
